3c697b419d7cfa9dc686dbbae682872305b0aa32
longformist-gareth <gareth@longformist.co.uk>
2019-07-09 11:41:26 +0100

    Update boris.wanker.user.js

--- boris.wanker.user.js
+++ boris.wanker.user.js
@@ -2,7 +2,7 @@
 // @name         Boris is a wanker
 // @namespace    https://github.com/longformist-gareth/boris-wanker/
 // @version      0.1
-// @description  Boris Johnson, wanking furiously, is a wanker. Make that clear any time you see him mentioned.
+// @description  Boris Johnson is a wanker. Make that clear any time you see him mentioned.
 // @author       John Bull
 // @match        https://*/*
 // @include     https://*/*
