e66c27416ad1fa88bf0a1030a9b69d1b304734cc
longformist-gareth <gareth@longformist.co.uk>
2019-07-09 08:52:29 +0100

    Update wanking-furiously.js
    changed namespace

--- wanking-furiously.js
+++ wanking-furiously.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Boris is a wanker
-// @namespace    http://localhost
+// @namespace    https://github.com/longformist-gareth/boris-wanker/
 // @version      0.1
-// @description  Boris Johnson is a wanker. Make that clear any time you see him mentioned.
+// @description  Boris Johnson, wanking furiously, is a wanker. Make that clear any time you see him mentioned.
 // @author       John Bull
 // @match        https://*/*
 // @include     https://*/*
@@ -14,7 +14,7 @@
     console.log(document.body.innerHTML);
 
 
-document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson,/g, 'Boris Johnson, wanking furiously, ');
-document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson\./g, 'Boris Johnson, wanking furiously. ');
-document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson /g, 'Boris Johnson, wanking furiously, ');
+document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson, wanking furiously, /g, 'Boris Johnson, wanking furiously,  wanking furiously, ');
+document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson\./g, 'Boris Johnson, wanking furiously,  wanking furiously. ');
+document.body.innerHTML = document.body.innerHTML.replace(/Boris Johnson, wanking furiously, /g, 'Boris Johnson, wanking furiously,  wanking furiously, ');
 })();
